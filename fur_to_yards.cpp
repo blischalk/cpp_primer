9a7b56bc5321738eb965ebf6afc2788b9dc0f757
Brett Lischalk <brett@brettlischalk.com>
2013-08-21 23:24:58 -0500

    Output name program.  Furlongs to yards program.

---
+++ fur_to_yards.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "Enter a distance in furlongs..." << endl;
+  int furs;
+  cin >> furs;
+  cout << (furs * 220) << " yards." << endl;
+}
---
+++ name.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "Blischalk" << endl;
+  cout << "http://www.brettlischalk.com";
+  return 0;
+}
+  
