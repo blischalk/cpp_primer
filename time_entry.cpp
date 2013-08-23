e4ade5321cd6a3940b3d20f3fbafa645b340010f
Brett Lischalk <brett@brettlischalk.com>
2013-08-22 19:13:43 -0500

    Added time entry.

---
+++ time_entry.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "Enter the number of hours: ";
+
+  int hour;
+  cin >> hour;
+
+  cout << "Enter the number of minutes: ";
+  int min;
+  cin >> min;
+
+  cout << "Time: " << hour << ":" << min << endl;
+}
